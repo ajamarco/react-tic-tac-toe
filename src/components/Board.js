bacc995b0c66d1f276beb7d0813317f412352c6d
alexandre jamarco <alexandre.j@gmail.com>
2020-06-15 13:21:14 +0100

    added style to board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import Square from './Square'
 
+const style = {
+    border: '4px solid black',
+    borderRadius: "10px",
+    width: "250px", 
+    height: "250px",
+    margin: "0 auto",
+    display: "grid",
+    gridTemplate: "repeat(3, 1fr) / repeat(3, 1fr)"
+}
+
 function Board({squares, onClick}) {
     return (
-        <div>
+        <div style={style}>
             <Square value='1' onClick={() => onClick('value')}/>
             <Square value='2' onClick={() => onClick('value')}/>
             <Square value='3' onClick={() => onClick('value')}/>
