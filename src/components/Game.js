dc42c5bd52dc5595c2f1dbd04e71ba0705574f20
alexandre jamarco <alexandre.j@gmail.com>
2020-06-15 21:12:17 +0100

    added new game button and winner text

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,18 +2,26 @@ import React, {useState} from 'react'
 import Board from './Board'
 import {calculateWinner} from '../helpers'
 
+const styles = {
+    width: '200px',
+    margin: '20px auto'
+}
+
 function Game() {
     const [board, setBoard] = useState(Array(9).fill(null));
     const [xIsNext, setXisNext] = useState(true);
     const winner = calculateWinner(board);
 
     const handleClick = (i) => {
-        const boardCopy = [board];
-        if (boardCopy[i] === null || winner) return;
-        boardCopy[i] = xIsNext ? 'X': 'O';
-        setBoard(boardCopy);
-        setXisNext(!xIsNext);
-        
+        debugger;
+        const boardCopy = [...board];
+        if (winner) return;
+        else if (boardCopy[i] === null) {
+            boardCopy[i] = xIsNext ? 'X': 'O';
+            setBoard(boardCopy);
+            setXisNext(!xIsNext);
+        }
+
     }
 
     const jumpTo = () => {
@@ -21,13 +29,19 @@ function Game() {
     }
 
     const renderMoves = () => {
-
+        return(
+            <button onClick={() => setBoard(Array(9).fill(null))}>Start Game</button>
+        )
     }
 
     return (
-        <div>
+        <>
             <Board onClick={handleClick} squares={board}/>
-        </div>
+            <div style={styles}>
+                <p>{winner ? `Winner: ${winner}` : "Next Player: " + (xIsNext ? "X": "O")}</p>
+                {renderMoves()}
+            </div> 
+        </>
     )
 }
 
